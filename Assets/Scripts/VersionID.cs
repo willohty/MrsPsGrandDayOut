11a59fa861b8b868920619b21cacb0e84cf47a95
willohty <willohty@outlook.com>
2022-06-07 00:34:16 -0700

    Asset Reduction + Music Change
    The music has been changed to public domain music "Mushrooms" by Komiko found at the Free Music Archive.
    Also removed a lot of HD skyboxes that were not going to be used in this project.

--- Assets/Scripts/VersionID.cs
+++ Assets/Scripts/VersionID.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public static class VersionID
 {
     public static string build = "webGL";
-    public static string number = "0.1.1";
-    public static string previousnumber = "0.1.0;";
+    public static string number = "0.1.2";
+    public static string previousnumber = "0.1.1;";
+
+    //Update Notes
+    //0.1.1 Playable Level
+    //0.1.2 Music changed to Public Domain, Unused Files Removed
 }
